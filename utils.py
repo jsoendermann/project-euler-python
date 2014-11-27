ed8b4bee1d9c72b33c632daa717f596aeba87416
Jan Soendermann <jan.soendermann@googlemail.com>
2014-11-27 11:04:41 +0000

    Remove redundant conversion to list from utils

--- utils.py
+++ utils.py
@@ -2,8 +2,7 @@ from math import sqrt
 
 def sum_of_digits(n):
     str_ = str(n)
-    list_ = list(str_)
-    int_list = map(int, list_)
+    int_list = map(int, str_)
     sum_ = sum(int_list)
     return sum_
 
